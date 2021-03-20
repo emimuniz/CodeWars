c56b8592ecb3b8addf489927498bd93808c6708d
Emillyn Muniz <emillyn_muniz@hotmail.com>
2021-03-20 18:10:04 -0300

    Update RegexPasswordValidation.js

--- JS/RegexPasswordValidation.js
+++ JS/RegexPasswordValidation.js
@@ -4,7 +4,18 @@ At least six characters long
 contains a lowercase letter
 contains an uppercase letter
 contains a number
-Valid passwords will only be alphanumeric characters. *//
+Valid passwords will only be alphanumeric characters.
+
+
+/^
+  (?=.*\d)              // deve conter ao menos um dígito
+  (?=.*[a-z])           // deve conter ao menos uma letra minúscula
+  (?=.*[A-Z])           // deve conter ao menos uma letra maiúscula
+  (?=.*[$*&@#])         // deve conter ao menos um caractere especial
+  [0-9a-zA-Z$*&@#]{8,}  // deve conter ao menos 8 dos caracteres mencionados
+$/
+
+*//
 
 function validate(password) {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password);
